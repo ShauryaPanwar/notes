c51cab36fd5e128111bcfe0dfcb86a01e6905540
ShauryaPanwar <panwarshaurya11@gmail.com>
2023-01-23 16:59:41 +0530

    changes

--- api/views.py
+++ api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def getRoutes(request):
+
+    routes =[
+        {
+            'Endpoint' : '/notes/',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Returns array of notes'
+        },
+        {
+            'Endpoint' : '/notes/id',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Returns a single note objects'
+
+        },
+
+        {
+            'Endpoint' : '/notes/create',
+            'method' : 'POST',
+            'body' : {'body':""},
+            'description' : 'Creates new notes with data sent in post request'
+        },
+
+        {
+            'Endpoint' : '/notes/id/update/',
+            'method' : 'PUT',
+            'body' : {'body':""},
+            'description' : 'Creates existing notes with data sent in put request'
+        },
+
+        {
+            'Endpoint' : '/notes/id/delete/',
+            'method' : 'DELETE',
+            'body' : None,
+            'description' : 'Deletes existing notes'
+        }
+    ]
+    return JsonResponse(routes ,safe=False)
