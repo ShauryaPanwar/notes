6a3f2a590ba6be3c772ac9167c8544b3e5176483
ShauryaPanwar <panwarshaurya11@gmail.com>
2023-01-23 19:29:04 +0530

    working rest framework

--- api/views.py
+++ api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a list of notes'
+        },
+        {
+            'Endpoint': '/notes/id/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ''},
+            'description': 'Creates a new note with data sent by post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ''},
+            'description': 'Updates a note with data sent by put request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes a note'
+        }
+    ]
+    return Response(routes)
