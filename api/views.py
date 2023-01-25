ff210c199c5bc460bbc5379ad145d0cb572d7cff
ShauryaPanwar <panwarshaurya11@gmail.com>
2023-01-25 18:01:27 +0530

    serialized

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializer import NoteSerializer
+from .models import Note
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -38,3 +39,10 @@ def getRoutes(request):
         }
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
